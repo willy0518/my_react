38d5569bedb78fbca74e92a02f3d871fb087498f
willy0518 <willy008093@gmail.com>
2020-02-13 10:49:24 +0800

    example0213

--- src/App.js
+++ src/App.js
@@ -1,54 +1,110 @@
-import React, { useState } from 'react';
+import React, { Component } from 'react';
 import './App.css';
 import Person from './Person/Person';
 
-const App = props => {
-  const [personState, setPersonsState] = useState({  //useState回傳兩個elements
+
+class App extends Component {
+  state = {
     persons: [
-      { name: "MAX", age: 28 },
-      { name: "Willy", age: 24 },
-      { name: "Will", age: 26 }
+      { id: 'aaaaa', name: 'Max', age: 28 },
+      { id: 'bbbbb', name: 'Manu', age: 29 },
+      { id: 'ccccc', name: 'Stephanie', age: 26 }
+    ],
+    otherState: 'some other value',
+    showPersons: false
+  }
 
-    ]
-  });
-
-  const [otherState, setOtherState] = useState('some other value');
-
-  console.log(personState, otherState);
-
-  const switchNameHandler = () => {
-    setPersonsState({
-
+  /*
+  switchNameHandler = (newName) => {
+    // console.log('Was clicked!');
+    // DON'T DO THIS: this.state.persons[0].name = 'Maximilian';
+    this.setState({
       persons: [
-        { name: 'Maximilian', age: 28 },
-        { name: 'Manu', age: 25 },
-        { name: 'Jason', age: 24 }
+        { name: newName, age: 28 },
+        { name: 'Manu', age: 29 },
+        { name: 'Stephanie', age: 27 }
       ]
+    })
+  }*/
+
+  deletePersonHandler = (personindex) => {
+    //const persons = this.state.persons.slice; 轉陣列舊方法
+    const persons = [...this.state.persons]; //es6 轉為陣列方法better
+    persons.splice(personindex, 1); //delete index
+    this.setState({ persons, persons })
+
+  }
+
+  nameChangedHandler = (event, id) => {
+    const personIndex = this.state.persons.findIndex(p => {
+      return p.id === id;
+
     });
-  };
+
+    const person = {
+      ...this.state.persons[personIndex]
+    };
+
+    person.name = event.target.value;
+
+    const persons = [...this.state.persons];
+
+    persons[personIndex] = person;
+
+    this.setState({ persons: persons })
+  }
+
+  togglePersonsHandler = (event) => {
+    const doesshow = this.state.showPersons;
+    this.setState({ showPersons: !doesshow })
+  }
+
+  // render執行後真正呈現在畫面上
+  render() {
+    const style = {
+      backgroundcolor: 'red',
+      font: 'inherit',
+      border: '1px solid blue',
+      padding: '8px',
+      cursor: 'pointer'
+
+
+    };
+    let persons = null;
+
+    if (this.state.showPersons) {
+      persons = (
+        <div>
+          {this.state.persons.map((person, index) => {
+            return <Person
+              click={() => this.deletePersonHandler(index)}
+              name={person.name}
+              age={person.age}
+              key={person.id}
+              changed={(event) => this.nameChangedHandler(event, person.id)} />
+          })}
+        </div>
+      );
+    }
+
+    return (
+      <div className="App">
+        <h1>Hi, I'm a React App</h1>
+        <p>This is really working!</p>
+        <button
+          style={style}
+          onClick={this.togglePersonsHandler}>Switch Name
+        </button>
+        {persons}
 
 
 
-  return (
-    <div className="App">
-      <h1>hi, I'm a react app</h1>
-      <p>this is really working</p>
-      <button onClick={switchNameHandler}>Switch name</button>
-      <Person
-        name={personState.persons[0].name}
-        age={personState.persons[0].age}>
-      </Person>
-      <Person
-        name={personState.persons[1].name}
-        age={personState.persons[1].age}>
-      </Person>
-      <Person
-        name={personState.persons[2].name}
-        age={personState.persons[2].age}>
-      </Person>
-    </div>
-  );
-};
 
+
+      </div>
+    );
+    // return React.createElement('div', {className: 'App'}, React.createElement('h1', null, 'Does this work now?'));
+  }
+}
 
 export default App;
